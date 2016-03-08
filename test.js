281fe6804a00ce522563e1e80383992650cfe018
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-03-07 22:26:46 -0800

    Add a few tests

--- test.js
+++ test.js
@@ -1,9 +1,35 @@
 var ExpirableHashTable = require('./index')
-var test = require('tape')
+var tape = require('tape')
 
-test('Should', function (t) {
+var table = new ExpirableHashTable(1000)
 
-
+tape('Should add item and allow it get by get by key', function (t) {
+  table.set('key', 'value')
+  t.same(table.get('key'), 'value')
   t.end()
+})
 
-});
+tape('Should remove item after expirety', function (t) {
+  table.set('key', 'value')
+  setTimeout(function () {
+    t.same(table.get('key'), undefined)
+    t.end()
+  }, 1000)
+    
+})
+
+tape('Should not remove item before expirery', function (t) {
+  table.set('key', 'value')
+  setTimeout(function () {
+    t.same(table.get('key'), 'value')
+    t.end()
+  }, 900)
+})
+
+tape('Should support custom expirery', function (t) {
+  table.set('key', 'value', 500)
+  setTimeout(function () {
+    t.same(table.get('key'), undefined)
+    t.end()
+  }, 550)
+})
